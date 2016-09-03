2fa1691355d1d4d70d9a6ecdaec5eb2180dde24c
Alex Howard <thezanke@gmail.com>
2016-09-03 02:57:05 -0400

    Handle image preloading

--- index.js
+++ index.js
@@ -21,19 +21,36 @@ export default class HeightMatchingGroup extends Component {
 
   componentDidMount() {
     window.addEventListener('resize', this.matchHeights);
+    this.loadImages().then(this.matchHeights);
     setTimeout(this.matchHeights, 0);
   }
 
-  componentDidUpdate() {
-    setTimeout(this.matchHeights, 0);
-  }
+  componentDidUpdate() { setTimeout(this.matchHeights, 0); }
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.matchHeights);
-  }
+  componentWillUnmount() { window.removeEventListener('resize', this.matchHeights); }
 
   getContainerRef = node => { this.container = node; }
+
   getMaxHeight = els => Array.prototype.map.call(els, el => el.scrollHeight).reduce((pre, cur) => Math.max(pre, cur), -Infinity);
+
+  loadImages = () => {
+    const p = new Promise((resolve) => {
+      const images = this.container.querySelectorAll('img');
+
+      let loaded = 0;
+
+      const handleImage = () => {
+        loaded += 1;
+        if (loaded === images.length) resolve();
+      };
+
+      images.forEach(image => {
+        image.onload = handleImage;
+      });
+    });
+
+    return p;
+  };
 
   matchHeights = () => {
     const els = this.container.querySelectorAll(this.props.selector);
